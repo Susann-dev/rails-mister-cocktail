d9b9e40b1f519148856076259882c8f32ad0d084
susann kaechler <sanni51@gmx.de>
2021-01-16 16:53:02 +0100

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses, only: [ :new, :create ]
+  end
+
+  get "cocktails", to: "cocktails#index"
+  get "cocktails/new", to: "cocktails#new"
+  post "cocktails", to: "cocktails#create"
+  get "cocktails/:id", to: "cocktails#show"
+  get "/cocktails/:cocktail_id/doses/new", to: "doses#new"
+  post "/cocktails/:cocktail_id/doses", to: "doses#create"
+
 end
+
